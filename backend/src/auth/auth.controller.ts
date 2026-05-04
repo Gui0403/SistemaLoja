60574d1af53856f8b28cd673d8e20f887d3cb067
Gui0403 <pabloguilhermemartinez@gmail.com>
2026-05-04 07:26:35 -0400

    Fetch: auth e init da integração com prisma

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import type { SignInDTO, SignUpDTO } from './dtos/auth';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('signup')
+    async signup(@Body() body: SignUpDTO) {
+        await this.authService.signup(body);
+
+        return 'signup';
+    }
+
+    @Post('signin')
+    async signin(@Body() body: SignInDTO) {
+        await this.authService.signin(body);
+
+        return 'signin';
+    }
+}
