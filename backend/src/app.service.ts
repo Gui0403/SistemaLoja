2ae0580304303e9de1b95099d88d43c24ab1a6af
Gui0403 <pabloguilhermemartinez@gmail.com>
2026-05-17 12:02:31 -0400

    Fix: alias @

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHome(): string {
-    return 'home Page successful!';
+  getHello(): string {
+    return 'Hello World';
   }
 }
