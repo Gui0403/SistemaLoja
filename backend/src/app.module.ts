b49ffe510b13bb004aea76f938657030b9f82865
Gui0403 <pabloguilhermemartinez@gmail.com>
2026-05-17 11:41:03 -0400

    Fix: include tsconfig

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '@/app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '@/auth/auth.module';
 import { AppController } from '@/app.controller';
